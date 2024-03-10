f5e7c35d5685f87172871c071b3d82277a4fc5df
TEST <test@TESTs-MacBook-Pro.local>
2024-03-10 12:46:13 +0000

    Apps missed a comma

--- Medium/settings.py
+++ Medium/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['medium-410cf7fad2b1.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['medium-410cf7fad2b1.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
