6081308ce1151e0cde3a2801c07ead90bbc39f98
Xalil404 <kareemxalil@gmail.com>
2025-10-26 13:19:56 +0000

    Added new DB

--- Medium/settings.py
+++ Medium/settings.py
@@ -30,8 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 
 ALLOWED_HOSTS = ['medium-410cf7fad2b1.herokuapp.com', 'localhost', '127.0.0.1']
